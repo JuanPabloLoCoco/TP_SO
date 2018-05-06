466b2f6a04c0018d345b815ed23223c577f80d96
JuanPabloLoCoco <jlococo@itba.edu.ar>
2018-05-06 16:54:20 -0300

    pipes.c y .h

---
+++ TP_Arquitectura_De_Computadoras-master/RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/pipe.h
@@ -0,0 +1,34 @@
+#ifndef _PIPE_H
+#define _PIPE_H
+
+#include <stdint.h>
+#include "condvar.h"
+
+#include "IPCstructs.h"
+#define MAX_PIPES 100
+#define PIPES_MUTEX "__PIPES__MUTEX"
+
+typedef struct{
+    char* buffer;
+    int bufferSize;
+    int initialIndex;
+    int mutex;
+    int readMutex;
+    int writeMutex;
+    condVar_t readCondVar;
+    condVar_t writeCondVar;
+    char* name;
+}pipe_s;
+
+typedef pipe_s* pipe_t;
+
+pipe_t getPipe(char* name);
+void releasePipe(char* name);
+int writePipe(pipe_t pipe,char* msg , uint64_t amount);
+int readPipe(pipe_t pipe,char* ans,uint64_t amount);
+void initIPC();
+
+int getPipesNames(ipcs* ans,int cant);
+
+#define MAX_PIPE_NAME 15
+#endif
