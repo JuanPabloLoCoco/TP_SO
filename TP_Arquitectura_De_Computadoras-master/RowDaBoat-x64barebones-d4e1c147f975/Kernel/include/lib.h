f7af590f39c160e07ed844b6a24cbb0770674f5c
JuanPabloLoCoco <jlococo@itba.edu.ar>
2018-05-06 11:52:31 -0300

    Subo defines

--- TP_Arquitectura_De_Computadoras-master/RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/lib.h
+++ TP_Arquitectura_De_Computadoras-master/RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/lib.h
@@ -8,17 +8,19 @@ void * memcpy(void * destination, const void * source, uint64_t length);
 
 char *cpuVendor(char *result);
 
+#define MEMBEGIN 0x10000000
+#define MAXMEMORY (128*1024*1024) //in b//
+#define MINPAGE (4*1024) //in b//
+
+#define PAGE_SIZE 4*1024
+
 int getSeconds(void);
-
 int getMinutes(void);
-
 int getHours(void);
-
 int getYear(void);
-
 int getMonth(void);
-
 int getDay(void);
+
 
 void readKeyboard(void);
 
@@ -26,7 +28,7 @@ void setup_IDT_entry (int index, uint64_t offset);
 
 void timerTickHandler(void);
 
-void setUPTimerTickHandler(void); 
+void setUPTimerTickHandler(void);
 
 
 #endif
