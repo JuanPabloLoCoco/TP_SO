f936450529214a796eecbf594e97fa527e6bde0b
JuanPabloLoCoco <jlococo@itba.edu.ar>
2018-05-06 13:14:09 -0300

    subo actualizaciones buddy allocator

--- TP_Arquitectura_De_Computadoras-master/RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/buddyAllocator.h
+++ TP_Arquitectura_De_Computadoras-master/RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/buddyAllocator.h
@@ -7,8 +7,10 @@ void initializeHeap();
 void* buddyAllocatePages(uint64_t pages);
 void* buddyAllocate(uint64_t amount);
 int buddyFree(void* address);
+
 uint16_t myBit(uint16_t n);
 uint16_t myMask(uint16_t n);
+
 int isPowerOfTwo (uint64_t x);
 uint64_t roundUpPower2(uint64_t v);
 
--- TP_Arquitectura_De_Computadoras-master/RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/math.h
+++ TP_Arquitectura_De_Computadoras-master/RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/math.h
@@ -1,3 +1,4 @@
 int isPowerOfTwo (uint64_t x);
 uint64_t roundUpPower2(uint64_t v);
-uint64_t pow2(uint64_t n);
+uint64_t pow2(uint64_t n;
+int lowerBound2Pow(int n);
