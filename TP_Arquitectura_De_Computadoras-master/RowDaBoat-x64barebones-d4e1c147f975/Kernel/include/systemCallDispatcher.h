b2c5978f7dc3f141c4b9872303ff88bd4f061ab6
JuanPabloLoCoco <jlococo@itba.edu.ar>
2018-05-09 12:45:58 -0300

    Agrego SysCalls

--- TP_Arquitectura_De_Computadoras-master/RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/systemCallDispatcher.h
+++ TP_Arquitectura_De_Computadoras-master/RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/systemCallDispatcher.h
@@ -26,5 +26,19 @@ uint64_t sys_free(uint64_t toFree);
 uint64_t sys_leave();
 uint64_t sys_ps(uint64_t buffer, uint64_t size);
 uint64_t sys_sleep(uint64_t time);
-
+uint64_t sys_yield();
+uint64_t sys_exec(uint64_t entry_point, uint64_t pid, uint64_t cargs, uint64_t pargs);
+uint64_t sys_kill(uint64_t pid, uint64_t msg);
+uint64_t sys_myPID();
+uint64_t sys_openPipe(uint64_t name,uint64_t ans);
+uint64_t sys_closePipe(uint64_t name);
+uint64_t sys_waitSemaphore(uint64_t semaphoreArg, uint64_t mutexArg);
+uint64_t sys_signalSemaphore(uint64_t semaphoreArg);
+uint64_t sys_initSemaphore(uint64_t semaphoreArg);
+uint64_t sys_broadcastSemaphore(uint64_t semaphoreArg);
+uint64_t sys_ipcs(uint64_t pipcs, uint64_t mutexes, uint64_t pipes, uint64_t ansMutexes, uint64_t ansPipes);
+uint64_t sys_createMutex(uint64_t name, uint64_t mutex) ;
+uint64_t sys_releaseMutex(uint64_t mutex,uint64_t ret) ;
+uint64_t sys_tryLock(uint64_t mutex,uint64_t ret);
+uint64_t sys_unlock(uint64_t mutex, uint64_t ret);
 #endif
--- TP_Arquitectura_De_Computadoras-master/RowDaBoat-x64barebones-d4e1c147f975/Kernel/systemCallDispatcher.c
+++ TP_Arquitectura_De_Computadoras-master/RowDaBoat-x64barebones-d4e1c147f975/Kernel/systemCallDispatcher.c
@@ -4,6 +4,10 @@
 #include <buddyAllocator.h>
 #include <scheduler.h>
 #include <time.h>
+#include <semaphore.h>
+#include <mutex.h>
+
+
 
 uint64_t systemCallDispatcher(uint64_t rax, uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t r8, uint64_t r9)
 {
@@ -65,7 +69,52 @@ uint64_t systemCallDispatcher(uint64_t rax, uint64_t rdi, uint64_t rsi, uint64_t
 				result = sys_ps(rdi, rsi);
 				break;
 			case 18:
-				result= sys_sleep(rdi);
+				result = sys_sleep(rdi);
+				break;
+			case 19:
+				result = sys_yield();
+				break;
+			case 20:
+				result = sys_exec(rdi, rsi,rdx, r8);
+				break;
+			case 21:
+				result = sys_kill(rdi, rsi);
+				break;
+			case 22:
+				result = sys_myPID();
+				break;
+			case 23:
+				result = sys_openPipe(rdi,rsi);
+				break;
+			case 24:
+				result = sys_closePipe(rdi);
+				break;
+			case 25:
+				result = sys_signalSemaphore(rdi);
+				break;
+			case 26:
+				result = sys_waitSemaphore(rdi,rsi);
+				break;
+			case 27:
+				result = sys_initSemaphore(rdi);
+				break;
+			case 28:
+				result = sys_broadcastSemaphore(rdi);
+				break;
+			case 29:
+				result = sys_ipcs(rdi,rsi,rdx,r8,r9);
+				break;
+			case 30:
+				result = sys_createMutex(rdi,rsi);
+				break;
+			case 31:
+				result = sys_releaseMutex(rdi, rsi);
+				break;
+			case 32:
+				result = sys_tryLock(rdi,rsi);
+				break;
+			case 33:
+				result = sys_unlock(rdi,rsi);
 				break;
 		}
 		return result;
@@ -223,4 +272,131 @@ uint64_t sys_sleep(uint64_t time)
 {
 	sleep(time);
   return 0;
+}
+
+uint64_t sys_yield()
+{
+    _yield();
+    return 0;
+}
+
+uint64_t sys_exec(uint64_t entry_point, uint64_t pid, uint64_t cargs, uint64_t pargs)
+{
+    int * retPid = (int *) pid;
+    *retPid = insertProcess((void *)entry_point, (int)cargs, (void **)pargs);
+    return 0;
+}
+
+uint64_t sys_kill(uint64_t pid, uint64_t msg)
+{
+    switch(msg)
+		{
+        case 0:
+            // kill custom process
+            changeProcessState(pid, DEAD);
+            break;
+        case 1:
+            // sleep custom process
+            changeProcessState(pid, SLEEPING);
+            break;
+        case 2:
+            // wake up custom process
+            changeProcessState(pid, READY);
+            break;
+        case 3:
+            setForeground(pid);
+            break;
+    }
+    return 0;
+}
+
+uint64_t sys_myPID()
+{
+    // int * retPid = (int *) ans;
+    // *retPid = getCurrentPid();
+    return getCurrentPid();
+}
+
+uint64_t sys_openPipe(uint64_t name,uint64_t ans) {
+  	int * a = (int *) ans;
+  	*a = addPipe(getPipe((char*)name)) +3;
+  	return 0;
+}
+
+uint64_t sys_closePipe(uint64_t name) {
+    releasePipe(name);
+    return;
+}
+
+uint64_t sys_waitSemaphore(uint64_t semaphoreArg, uint64_t mutexArg)
+{
+    semaphore_t * semaphore = (semaphore_t *) semaphoreArg;
+    int mutex = (int ) mutexArg;
+    waitSemaphore(semaphore,mutex);
+		return 0;
+}
+
+uint64_t sys_signalSemaphore(uint64_t semaphoreArg)
+{
+    semaphore_t * semaphore = (semaphore_t *) semaphoreArg;
+    signalSemaphore(semaphore);
+    return;
+}
+
+uint64_t sys_initSemaphore(uint64_t semaphoreArg)
+{
+    semaphore_t * semaphore = (semaphore_t *) semaphoreArg;
+    initSemaphore(semaphore);
+    return;
+}
+
+uint64_t sys_broadcastSemaphore(uint64_t semaphoreArg)
+{
+    semaphore_t * semaphore = (semaphore_t *) semaphoreArg;
+    broadcastSemaphore(semaphore);
+    return;
+}
+
+uint64_t sys_ipcs(uint64_t pipcs, uint64_t mutexes, uint64_t pipes,uint64_t ansMutexes,uint64_t ansPipes)
+{
+    int *ansM = (int *) ansMutexes;
+    int *ansP = (int *) ansPipes;
+    ipcs *ans = (ipcs *) pipcs;
+    *ansM = getMuxeseNames(ans, mutexes);
+    *ansP = getPipesNames(ans, pipes);
+    return;
+}
+
+
+/*------------------------MUTEX ----------------------*/
+uint64_t sys_createMutex(uint64_t name, uint64_t mutex)
+{
+    int * ret = (int *) mutex;
+    *ret = getMutex((char*) name);
+    return 0;
+}
+
+uint64_t sys_releaseMutex(uint64_t mutex,uint64_t ret)
+{
+    int * retVal = (int *) ret;
+    int mutexCode = (int ) mutex;
+    *retVal = releaseMutexFromPos(mutexCode);
+    return 0;
+}
+
+
+uint64_t sys_tryLock(uint64_t mutex,uint64_t ret)
+{
+    int * retVal = (int *) ret;
+    int mutexCode = (int ) mutex;
+    *retVal = lockMutex(mutexCode);
+    return 0;
+}
+
+uint64_t sys_unlock(uint64_t mutex, uint64_t ret)
+{
+    int * retVal = (int *) ret;
+    int mutexCode = (int ) mutex;
+    *retVal = unlockMutex(mutexCode);
+    return 0;
 }
